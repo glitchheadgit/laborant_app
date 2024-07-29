602e95086a56814d8fb6c41c12752875adfac10e
Ustin Zolotikov <zolotikov.u.e@yandex.ru>
2024-07-29 21:35:12 +0300

    0.0.2

--- handlers/questionaire.py
+++ handlers/questionaire.py
@@ -1,12 +1,14 @@
 import torch
+import io
 
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, BufferedInputFile
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 
 from utils.states import Docs
-from utils.preprocessing import read_docx, read_pdf, format_with_yangex_gpt
+from utils.model import format_analyses, add_state, predict_diagnoses
+from utils.preprocessing import read_docx, read_pdf
 from keyboards import reply
 
 
@@ -21,28 +23,58 @@ async def get_photo(message: Message, state: FSMContext):
 
 @router.message(Docs.user_analyses, F.document.file_name.endswith('.pdf'))
 async def process_pdf(message: Message, state: FSMContext):
-    file_id = message.document.file_id
-    file = await message.bot.download(file_id)
-    text = read_pdf(file)
-    result = format_with_yangex_gpt(text)
-    await message.reply(
-        result,
-        reply_markup=reply.main,
+    await message.answer(
+        'Подождите минуту, Ваши анализы обрабатываются...',
     )
-    await state.clear()
+    file_id = message.document.file_id
+    try:
+        file = await message.bot.download(file_id)
+        text = read_pdf(file)
+        with io.BytesIO() as log:	
+            analyses = format_analyses(text)
+            log.write(analyses.encode('utf-8'))
+            state = add_state(analyses)
+            log.write(state.encode('utf-8'))
+            result =  predict_diagnoses(state)
+            result = result.replace('&', '&amp').replace('<', '&lt;').replace('>', '&gt;')
+            await message.reply_document(
+                document=BufferedInputFile(log, filename='log.txt'),
+                caption='<pre><code>' + result + '</code></pre>',
+                reply_markup=reply.main,
+            )
+    except Exception as e:
+        await message.reply(
+            '<pre><code>' + str(e).replace('&', '&amp').replace('<', '&lt;').replace('>', '&gt;') + '</code></pre>',
+            reply_markup=reply.main
+        )
 
 
 @router.message(Docs.user_analyses, F.document.file_name.regexp('.*docx?'))
 async def process_docx(message: Message, state: FSMContext):
-    file_id = message.document.file_id
-    file = await message.bot.download(file_id)
-    text = read_docx(file)
-    result = format_with_yangex_gpt(text)
-    await message.reply(
-        result,
-        reply_markup=reply.main,
+    await message.answer(
+            'Подождите минуту, Ваши анализы обрабатываются...',
     )
-    await state.clear()
+    file_id = message.document.file_id
+    try:
+        file = await message.bot.download(file_id)
+        text = read_docx(file)
+        with io.BytesIO() as log:
+            analyses = format_analyses(text)
+            log.write(analyses.encode('utf-8'))
+            state = add_state(analyses)
+            log.write(state.encode('utf-8'))
+            result =  predict_diagnoses(state)
+            result = result.replace('&', '&amp').replace('<', '&lt;').replace('>', '&gt;')
+            await message.reply_document(
+                document=BufferedInputFile(log, filename='log.txt'),
+                caption='<pre><code>' + result + '</code></pre>',
+                reply_markup=reply.main,
+            )
+    except Exception as e:
+        await message.reply(
+            '<pre><code>' + str(e).replace('&', '&amp').replace('<', '&lt;').replace('>', '&gt;') + '</code></pre>',
+            reply_markup=reply.main
+        )
 
 
 @router.message(CommandStart())
@@ -50,6 +82,10 @@ async def process_start_command(message: Message, state: FSMContext):
     await state.clear()
     await message.answer("Чего желаете?", reply_markup=reply.main)
 
+@router.message(F.text == "Отменить запрос")
+async def process_start_command(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer("Чего желаете?", reply_markup=reply.main)
 
 @router.message(Docs.user_analyses, ~F.document.file_name.regexp(r'.*.pdf|.*.docx?'))
 async def wrong_format_handler(message: Message, state: FSMContext):
--- utils/model.py
+++ utils/model.py
@@ -1,42 +1,133 @@
+from utils.preprocessing import read_pdf
+import transformers
 import torch
-import random
 
-from pathlib import Path
-from torchvision import transforms
-import numpy as np
-from PIL import Image
+MODEL_ID = "meta-llama/Meta-Llama-3-8B-Instruct"
 
-DIAGNOSIS = {0: "No retinopathy", 1: "Mild", 2: "Moderate", 3: "Severe", 4: "Proliferative"}
+def format_analyses(user_input: str) -> str:
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=MODEL_ID,
+        model_kwargs={"torch_dtype": torch.bfloat16},
+        device_map="auto",
+    )
+    messages = [
+    {"role": "system", "content": """You are a text formatter, you get a text in a free form with laboratory analyses of patients, you must transform it into strings like 'ANALYSIS_NAME has a value of ANALYSIS_VALUE, it measured in UNIT_OF_MEASURE and has a reference range of normal values between MINIMUM VALUE (if missing = 0) and MAXIMUM VALUE (if missing = Infinity).' Dont provide any additional text beside strings with analyses information, it's important!
+    """},
+    {"role": "user", "content": user_input}
+    ]
 
-def prediction_wrapper(model, image, device=torch.device("cpu")):
-    image = Image.open(image)
-    with torch.no_grad():
-        transform = transforms.Compose(
-            [
-                transforms.Resize(size=(224, 224)),
-                transforms.ToTensor(),
-                transforms.Normalize(
-                    [0.4064, 0.2169, 0.0722], [0.2786, 0.1515, 0.0811]
-                ),
-            ]
-        )
-        image_tensor = transform(image)
-        image_sample = image_tensor.unsqueeze(0)
-        image_sample = image_sample.to(device)
-        model.eval()
-        logit = model(image_sample).cpu()
-        probs = torch.nn.functional.softmax(logit, dim=-1).numpy()
-        prob = np.max(probs)
-        name_class = np.argmax(probs)
-    return f'{DIAGNOSIS[name_class]}', prob
+    terminators = [
+    pipeline.tokenizer.eos_token_id,
+    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    ]
+    outputs = pipeline(
+        messages,
+        max_new_tokens=3000,
+        eos_token_id=terminators,
+        do_sample=True,
+        temperature=0.6,
+        top_p=0.9,
+    )
+    with open('log_analyses.txt', 'w') as f:
+        f.write(user_input)
+        f.write('\n')
+        f.write(str(outputs[0]["generated_text"]))
+    return (outputs[0]["generated_text"][-1]['content']).replace('<', ' less than ').replace('>', ' greater than ')
 
 
-def choose_image_by_stage(stage):
-    if stage == 5:
-        n = len(DIAGNOSIS)
-        stage = random.randint(1, n)
-    p = Path('data/images')
-    images = list(p.glob(f'{stage}*'))
-    idx = random.choice(range(len(images)-1))
-    return str(images[idx].absolute())
+def add_state(analyses: str) -> str:
+    print(MODEL_ID)
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=MODEL_ID,
+        model_kwargs={"torch_dtype": torch.bfloat16},
+        device_map="auto",
+    )
+    messages = [
+    {"role": "system", "content": """You are a text analyser, you will get a bunch os sentances about results of lab analyses. Please return a short summary in this format: 'Analysis name - State'. 'State' is categorical and can have only 3 values: 'Low', 'High', 'Normal'. Decimal separator in text is a point. If you dont see reference range in recieved text, please, try to evaluate state on your own. Dont provide any additional text besides summary, it's important!
+    Examples: 
+    Input:
+    Kek has a value of 40.7, it measured in мкмоль/л and has a reference range of normal values 5.8 - 50.5.
+    // Kek value is greater than minimum normal reference (5.8) and less than maximum normal reference (50.5) so the state should be Normal
+    Output:
+    Kek - Normal
 
+    Input:
+    Lol has a value of 4.660, it measured in μIU/ml and has a reference range of normal values 0.27-4.2.
+    // Lol of 4.66 is bigger than the maximum normal reference (4.2) and bigger than the minimum normal reference (0.27) so the state should be 'High'
+    Output:
+    Lol - High
+
+    Input:
+    Krek has a value of 45.9, it measured in IU/l and has a reference range of normal values < 41.
+    // All Krek value less than 41 is normal, but we have a greater value of 45.9 so state should be 'High'
+    Output:
+    Krek - High
+
+    Input:
+    Experimental test has a value of 33.9, it measured in IU/l and has a reference range of normal values > 41.
+    // All experimental test value greater than 41 is normal, but we have a less value of 33.9 so state should be 'Low'
+    Output:
+    Experimental test - Low
+
+    Input:
+    Owo has a value of 31.6, it measured in IU/l and has a reference range of normal values < 40.
+    Uwu has a value of 0.89, it measured in % and has a reference range of normal values 0.8-1.14.
+    // Owo value less than 40 is normal and we have a value of 31.6 so state should be 'Normal'
+    // Uwu value must be greater than 0.8 and less than 1.14 so for 0.89 value State should be 'Normal'
+    Output:
+    Owo - Normal
+    Uwu - Normal
+    """},
+    {"role": "user", "content": analyses}
+    ]
+    terminators = [
+    pipeline.tokenizer.eos_token_id,
+    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    ]
+    outputs = pipeline(
+        messages,
+        max_new_tokens=3000,
+        eos_token_id=terminators,
+        do_sample=True,
+        temperature=0.6,
+        top_p=0.9,
+    )
+    with open('log_analyses.txt', 'w') as f:
+        f.write(analyses)
+        f.write('\n')
+        f.write(str(outputs[0]["generated_text"]))
+
+    return (outputs[0]["generated_text"][-1]['content'])
+
+
+def predict_diagnoses(analyses: str) -> str:
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=MODEL_ID,
+        model_kwargs={"torch_dtype": torch.bfloat16},
+        device_map="auto",
+    )
+    messages = [
+    {"role": "system", "content": f"Please analyze the user-provided CSV table containing laboratory test results. Columns of the data are the following: Analysis, Value, Reference minimum value, Reference maximum value, Unit of Measure. If the analysis values more than minimum reference value and less than reference maximum value, they are considered normal, in any other case it is abnormal! If You may provide a diagnosis if confident, suggest further tests if more information is needed, and recommend a specialist to consult if there are suspicions of any conditions beyond the current analysis. Please format this text to support sending by telegram bot with HTML Parser."},
+    {"role": "user", "content": analyses}
+    ]   
+    terminators = [
+    pipeline.tokenizer.eos_token_id,
+    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    ]
+    outputs = pipeline(
+        messages,
+        max_new_tokens=3000,
+        eos_token_id=terminators,
+        do_sample=True,
+        temperature=0.6,
+        top_p=0.9,
+    )
+    with open('log_diagnoses.txt', 'w') as f:
+        f.write(analyses)
+        f.write('\n')
+        f.write(str(outputs[0]["generated_text"][-1]['content']))
+
+    return (outputs[0]["generated_text"][-1]['content'])
